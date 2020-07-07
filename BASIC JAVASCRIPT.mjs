1ff447c80e655d452eb6c955a0c3f768914b2b05
Protec <virtualbenwaters@gmail.com>
2020-07-07 17:09:37 +0100

    Starting JS, learning variables types etc

---
+++ BASIC JAVASCRIPT.mjs
@@ -0,0 +1,31 @@
+BASIC JAVASCRIPT
+
+Two types of comments: 
+// Inline
+.
+/* not in-line... I cant remember what this is called lol
+*/
+.
+// JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+.
+// Variables can't start with a number or contain spaces
+{
+    /* You can easily increment or add one to a variable with the ++ operator.
+
+i++;
+
+is the equivalent of
+
+i = i + 1;
+} */
+
+{ /* You can easily decrement or decrease a variable by one with the -- operator.
+
+    i--;
+    
+    is the equivalent of
+    
+i = i - 1; */ }
+.
+
+
